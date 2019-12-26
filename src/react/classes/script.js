ab7a357ba87d34def614cd732fabb71724407be7
comradem <pperedot@gmail.com>
2019-12-26 11:38:19 +0200

    print numbers

--- src/react/classes/script.js
+++ src/react/classes/script.js
@@ -2,23 +2,38 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
-                <h1>Factorial</h1>
-                <Factorial/>
+                {/*<h1>Factorial</h1>*/}
+                {/*<Factorial/>*/}
+                <h1>Numbers</h1>
+                <ShowNumbers/>
             </div>
         );
     }
 }
 
+class ShowNumbers extends React.Component {
+    render() {
+        return (
+          <div>
+              <div>{`numbers are: ${this.getNumbers(12)} `}</div>
+          </div>
+        );
+    }
+    getNumbers(num){
+        return Array.from(Array(num), (elem, index)=> index+1);
+    }
+}
+
 class Factorial extends React.Component {
     render() {
-        return(
+        return (
             <div>factorial of 5 is: {this.calcFactorial(5)}</div>
         )
     }
 
-    calcFactorial(num){
-        if (num===1) return num;
-        return this.calcFactorial(num-1)*num;
+    calcFactorial(num) {
+        if (num === 1) return num;
+        return this.calcFactorial(num - 1) * num;
     }
 }
 
